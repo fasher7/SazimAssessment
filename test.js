0c70e4011adad93326be11e9d567f23d1f77e43e
Fahim Shahriar Eram <fasher37@gmail.com>
2024-05-11 20:15:28 +0600

    Completed

--- test.js
+++ test.js
@@ -1,27 +1,80 @@
-function mySearch()
-{
+document.getElementById("clearLocalStorageButton").addEventListener("click", function() {
+    localStorage.clear();
+    document.getElementById("showTask").innerHTML = "";
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadTasksFromLocalStorage();
+});
+
+function loadTasksFromLocalStorage() {
+    var storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+        var tasks = JSON.parse(storedTasks);
+        var outerArea = document.getElementById("showTask");
+        outerArea.innerHTML = ""; 
+
+        tasks.forEach(function(task, index) {
+            var innerArea = document.createElement("div");
+            innerArea.innerHTML = `
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="task_${index}" ${task.completed ? 'checked' : ''}>
+                    <label class="form-check-label" for="task_${index}">
+                        ${task.name}
+                    </label>
+                </div>`;
+            outerArea.appendChild(innerArea);
+
+            document.getElementById(`task_${index}`).addEventListener("change", function() {
+                updateTaskCompletionStatus(index, this.checked);
+            });
+
+            innerArea.addEventListener("click", function() {
+                removeTask(index);
+            });
+        });
+    }
+}
+
+function mySearch() {
     var taskName = document.getElementById("myInput").value;
-    outerArea = document.getElementById("showTask");
-    localStorage.setItem("tn", JSON.stringify(taskName))
+    var storedTasks = localStorage.getItem("tasks");
+    var tasks = storedTasks ? JSON.parse(storedTasks) : [];
+
+    tasks.push({ name: taskName, completed: false }); 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     document.getElementById("myInput").value = "";
 
+    var outerArea = document.getElementById("showTask");
     var innerArea = document.createElement("div");
-    innerArea.innerHTML = `<div class="form-check">
-    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-    <label class="form-check-label" for="flexCheckDefault">
-      ${taskName}
-    </label>
-    </div>`;
+    var index = tasks.length - 1;
+    innerArea.innerHTML = `
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="task_${index}">
+            <label class="form-check-label" for="task_${index}">
+                ${taskName}
+            </label>
+        </div>`;
     outerArea.appendChild(innerArea);
-    const userData = JSON.parse(localStorage.getItem('tn'));
+
+    document.getElementById(`task_${index}`).addEventListener("change", function() {
+        updateTaskCompletionStatus(index, this.checked);
+    });
+
+    innerArea.addEventListener("click", function() {
+        removeTask(index);
+    });
 };
 
-window.onbeforeunload = function() {
-    localStorage.setItem("tn", taskName);
-};
+function updateTaskCompletionStatus(index, completed) {
+    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    storedTasks[index].completed = completed;
+    localStorage.setItem("tasks", JSON.stringify(storedTasks));
+}
 
-window.onload = function() {
-    var name = localStorage.getItem("tn");
-    if (name !== null) $('#loadTask').val(name);
-    alert(name);
-};
+function removeTask(index) {
+    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    storedTasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(storedTasks));
+    loadTasksFromLocalStorage(); 
+}
